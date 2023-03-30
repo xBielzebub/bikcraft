c0a9b2047b5c22017d03bce9ee716c7a28d5e3cd
xBielzebub~ <gabrielhenrik.nunes@gmail.com>
2023-03-29 23:23:03 -0300

    galeria-de-imagens

--- js/script.js
+++ js/script.js
@@ -47,3 +47,22 @@ function eventosPerguntas(pergunta) {
 perguntas.forEach(eventosPerguntas);
 
 console.log(perguntas);
+
+// Galeria de imagens
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(evento) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
